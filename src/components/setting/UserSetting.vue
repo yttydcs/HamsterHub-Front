15f83028db02e5bc8d6951c7b200be78d3f0af6f
YTTY <1830286372@qq.com>
2024-04-16 17:23:04 +0800

    add user setting

---
+++ src/components/setting/UserSetting.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="userSettingBox settingBox">
+
+    <n-list hoverable bordered>
+      <template #header>
+        <n-thing title="用户设置" />
+      </template>
+
+      <n-list-item>
+        <input type="file" style="display:none" ref="uploadInput" accept="image/*" @change="handleFileUpload"/>
+        <n-thing title="头像" description="点击即可重新上传头像" />
+        <template #suffix>
+
+          <n-avatar
+              round
+              :size="36"
+              :src="loginData.user.avatarSrc"
+              :bordered="true"
+              @click="changeAvatar"
+          />
+
+        </template>
+      </n-list-item>
+
+      <n-list-item>
+        <n-thing title="密码" description="定时修改密码保证以密码安全" />
+        <template #suffix>
+
+          <n-button @click="()=>{changePwdShow = true}">
+            修改
+          </n-button>
+
+        </template>
+      </n-list-item>
+
+      <n-list-item>
+        <n-button class="logoutBtn" strong secondary type="error" @click="logout">
+          退出登陆
+        </n-button>
+      </n-list-item>
+
+    </n-list>
+    <!--  修改密码  -->
+    <n-modal
+        v-model:show="changePwdShow"
+        class="alertBox"
+        preset="card"
+        title="修改密码"
+        size="medium"
+    >
+      <n-form :model="formData">
+        <n-form-item-row label="旧密码">
+          <n-input v-model:value="formData.oldPwd"/>
+        </n-form-item-row>
+        <n-form-item-row label="新密码">
+          <n-input v-model:value="formData.newPwd"/>
+        </n-form-item-row>
+      </n-form>
+      <n-button type="primary" block secondary strong @click="confirmChangePwd">
+        提交
+      </n-button>
+    </n-modal>
+  </div>
+
+
+</template>
+
+<script>
+import {
+  NLayout,
+  NLayoutSider,
+  NH2,
+  NMenu,
+  NIcon,
+  NButton,
+  useThemeVars,
+  NListItem,
+  NList,
+  NThing,
+  NAvatar, NFormItemRow, NModal, NForm, NInput,
+} from "naive-ui";
+import {computed, h, nextTick, reactive, ref} from "vue";
+import loginData, {removeLoginData} from "@/common/loginData";
+import login from "@/api/login";
+
+
+export default {
+  name: 'userSetting',
+  components: {
+    NInput, NForm, NModal, NFormItemRow,
+    NAvatar,
+    NThing, NList, NListItem, NButton
+  },
+  props:{
+
+  },
+  methods:{
+    changeAvatar(){
+      this.$refs.uploadInput.click()
+    },
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      login.changeUserAvatar(file).then(res=>{
+        window.$message.success("上传成功，刷新后生效");
+      })
+    },
+    async confirmChangePwd(){
+      let that = this;
+      login.changePwd(this.formData.oldPwd,this.formData.newPwd).then(res=>{
+        if(res.code ===0){
+          removeLoginData();
+          that.changePwdShow = false;
+        }
+      })
+    },
+    logout(){
+      login.logout()
+    }
+  },
+
+  setup(){
+    let theme = useThemeVars();
+    return{
+      borderColor : computed(() => theme.value.borderColor),
+      borderHover : computed(() => theme.value.primaryColorHover),
+      borderSelected : computed(() => theme.value.primaryColorSuppl),
+      cubicBezierEaseInOut : theme.value.cubicBezierEaseInOut,
+      opacity2 : computed(() => theme.value.opacity2),
+      hoverColor : computed(() => theme.value.hoverColor),
+      loginData,
+      changePwdShow: ref(false),
+      formData:reactive({newPwd:"",oldPwd:""}),
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logoutBtn{
+  width: 100%;
+  height: 40px;
+}
+
+
+</style>
